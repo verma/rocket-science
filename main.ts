78540acdfd46d342d6d4003814a76f23880fff18
Uday Verma <uday.karan@gmail.com>
2016-09-24 23:02:44 -0500

    Add header comments

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+// main.ts
+//
+// To start the webpack dev server hit: Cmd+Shift+B (I think Cmd -> Ctrl for Windows).
+//
+// This is the entry point for your app, no real reason other than
+// we hook into jQuery's $() stuff here and launch app after the page load
+// finishes.
+//
+// Also includes reference to a demo class ScreenFlasher to show how to refer to other
+// source files.
+//
+// Good Luck ( ´ ▽ ` )b
+
 import * as $ from "jquery";
 
 import {ScreenFlasher} from "./src/utils.ts";
